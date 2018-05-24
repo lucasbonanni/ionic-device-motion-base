06d7b01e41072d7e63e79d6083e61c8f9795ea61
danag <le_bonanni@yahoo.com.ar>
2018-05-24 20:53:03 -0300

    primer commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,69 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { Content } from 'ionic-angular';
+import { Subscription } from 'rxjs/Subscription';
+import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(public navCtrl: NavController) {
 
+  subscription: Subscription;
+  @ViewChild('content') content: Content
+
+  public contentHeight: number;
+  public orientationX: number;
+  public orientationY: number;
+  public orientationZ: number;
+
+  constructor(public navCtrl: NavController, private deviceMotion: DeviceMotion, private platform: Platform) {
+
+  }
+
+
+  ngOnInit(): void {
+    // this.options = {
+    //   frequency: 300
+    // };
+    this.platform.ready().then(() => {
+      // Get the device current acceleration
+      this.deviceMotion.getCurrentAcceleration().then(
+        (acceleration: DeviceMotionAccelerationData) => {
+          this.orientationX = acceleration.x;
+          this.orientationY = acceleration.y;
+          this.orientationZ = acceleration.z;
+        },
+        (error: any) => alert(error)
+      );
+    });
+
+
+  }
+  ionViewDidEnter() {
+    this.platform.ready().then(() => {
+      // Watch device acceleration
+      this.subscription = this.deviceMotion.watchAcceleration({ frequency: 300 })
+        .subscribe((acceleration: DeviceMotionAccelerationData) => {
+          this.orientationX = acceleration.x;
+          this.orientationY = acceleration.y;
+          this.orientationZ = acceleration.z;
+        },
+          (error: any) => alert(error)
+        );
+    });
+  }
+
+  goBottom() {
+    console.log(this.content);
+
+    this.content.scrollToBottom(10000);
+  }
+
+  ionViewWillUnload() {
+    this.subscription.unsubscribe();
   }
 
 }
